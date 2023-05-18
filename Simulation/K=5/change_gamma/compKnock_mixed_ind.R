b4f8c6ca265d899c53e04c40c573057c318da0bc
Runqiu Wang <37877117+RunqiuWang22@users.noreply.github.com>
2023-05-18 18:18:29 +0800

    Add files via upload

---
+++ Simulation/K=5/change_gamma/compKnock_mixed_ind.R
@@ -0,0 +1,220 @@
+library("magic")
+library("mvtnorm")
+library("lars")
+library("knockoff")
+library("grpreg")
+library("far")
+library("methods")
+library("glmnet")
+library(seqknockoff)
+
+generateZ<-function(fit){
+  # p is the number of group
+  res<-abs(fit$beta[-1,])>0
+  gg <- as.numeric(fit$group)
+  N <- dim(res)[1]/2 #the number of predictors
+  p=max(gg)/2 #the number of group
+  np<-as.vector(table(gg))[1:p]#the number of predictors in each group
+  cnp<-c(0,cumsum(np))
+  
+  ###decide how many variables are nonzero within each group for each lamda
+  ee<-apply(res,2,cumsum)
+  dd<-ee[c(cnp[-1],cnp[-1]+N),]-rbind(0,ee[c(cnp[-(p+1)],cnp[-(p+1)]+N),])
+  ###find the best beta
+  tmpf<-function(v){
+    s<-0
+    a<-which(v>0)
+    if (length(a)>0){
+      s<-fit$lambda[min(a)]
+    }
+    s
+  }
+  tmp<-as.vector(apply(dd,1,tmpf))
+  #W<-(tmp[1:p]+tmp[p+1:p])*((tmp[1:p]-tmp[p+1:p])>0)-tmp[p+1:p]
+  tmp
+}
+
+####fitting
+
+myknockoff <- function(datal){
+	##method Max: sign max
+	##method Diff: difference
+  data<-datal$data
+  group<-datal$group1
+  
+  data1<-data[which(data$C==1),]
+  data2<-data[which(data$C==2),]
+  data3<-data[which(data$C==3),]
+  data4<-data[which(data$C==4),]
+  data5<-data[which(data$C==5),]
+ 
+  p=max(datal$group1) ### how many group
+  X1<-data1[,-c(1:2)]
+  X2<-data2[,-c(1:2)]
+  X3<-data3[,-c(1:2)]
+  X4<-data4[,-c(1:2)]
+  X5<-data5[,-c(1:2)]
+  
+  y1<-data1[,2]
+  y2<-data2[,2]
+  y3<-data3[,2]
+  y4<-data4[,2]
+  y5<-data5[,2]
+  
+  X1Knock<-knockoffs_seq(X1)
+  X2Knock<-knockoffs_seq(X2)
+  X3Knock<-knockoffs_seq(X3)
+  X4Knock<-knockoffs_seq(X4)
+  X5Knock<-knockoffs_seq(X5)
+  
+  ###group
+  which.factor <- as.numeric(which(sapply(X1, is.factor)))
+  LL <- nlevels(X1[,which.factor[1]]) ##the levels for each column are the same currently
+  group_cate=rep(group[which.factor],each=LL-1) ### if the group size are different, the number should be changed
+  groupx <- c(group_cate, group[-which.factor])
+  group <-groupx[order(groupx)]
+  
+  ### using group lasso for the coefficient
+  X1C <- model.matrix( ~ ., X1)[, -1]
+  X1KnockC <- model.matrix( ~ ., X1Knock)[, -1]
+  X2C <- model.matrix( ~ ., X2)[, -1]
+  X2KnockC <- model.matrix( ~ ., X2Knock)[, -1]
+  X3C <- model.matrix( ~ ., X3)[, -1]
+  X3KnockC <- model.matrix( ~ ., X3Knock)[, -1]
+  X4C <- model.matrix( ~ ., X4)[, -1]
+  X4KnockC <- model.matrix( ~ ., X4Knock)[, -1]
+  X5C <- model.matrix( ~ ., X5)[, -1]
+  X5KnockC <- model.matrix( ~ ., X5Knock)[, -1]
+  
+  data1X=cbind(y1,as.matrix(cbind(X1C,X1KnockC)))
+  data2X=cbind(y2,as.matrix(cbind(X2C,X2KnockC)))
+  data3X=cbind(y3,as.matrix(cbind(X3C,X3KnockC)))
+  data4X=cbind(y4,as.matrix(cbind(X4C,X4KnockC)))
+  data5X=cbind(y5,as.matrix(cbind(X5C,X5KnockC)))
+  
+  return(list(data1X=data1X,data2X=data2X,data3X=data3X,data4X=data4X,data5X=data5X,group=group))
+}
+
+### fit the model use two method: group lasso and lasso
+ffit = function(datal, choose){
+X1 = as.matrix(datal$data1X[,-1])
+y1 = datal$data1X[,1]
+
+X2 = as.matrix(datal$data2X[,-1])
+y2 = datal$data2X[,1]
+
+X3 = as.matrix(datal$data3X[,-1])
+y3 = datal$data3X[,1]
+
+X4 = as.matrix(datal$data4X[,-1])
+y4 = datal$data4X[,1]
+
+X5 = as.matrix(datal$data5X[,-1])
+y5 = datal$data5X[,1]
+  
+group=datal$group
+p=max(group)
+N=dim(X1)[2]/2 ## dimension of X after transfering into dummy variables
+
+if (choose=="grlasso"){
+  fit1<-grpreg(X1,y1,group=c(group,group+p),penalty="grLasso",family="gaussian",nlambda=1000)
+  fit2<-grpreg(X2,y2,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+  fit3<-grpreg(X3,y3,group=c(group,group+p),penalty="grLasso",family="gaussian",nlambda=1000)
+  fit4<-grpreg(X4,y4,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+  fit5<-grpreg(X5,y5,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+
+  Z1=abs(generateZ(fit1)[(1:p)])
+  Z1tilde=abs(generateZ(fit1)[p+(1:p)])
+  Z2=abs(generateZ(fit2)[(1:p)])
+  Z2tilde=abs(generateZ(fit2)[p+(1:p)])
+  Z3=abs(generateZ(fit3)[(1:p)])
+  Z3tilde=abs(generateZ(fit3)[p+(1:p)])
+  Z4=abs(generateZ(fit4)[(1:p)])
+  Z4tilde=abs(generateZ(fit4)[p+(1:p)])
+  Z5=abs(generateZ(fit5)[(1:p)])
+  Z5tilde=abs(generateZ(fit5)[p+(1:p)])
+  
+}
+
+if (choose=="lasso"){
+  cvfit1<-cv.glmnet(X1,y1,family="gaussian",standardize=FALSE,intercept=TRUE) 
+  fit1<-glmnet(X1,y1,family="gaussian",standardize=FALSE,intercept=TRUE,lambda=(cvfit1$lambda.min))
+  tmp1=as.numeric(coef(fit1))[-1]
+  
+  cvfit2<-cv.glmnet(X2,y2,family="binomial",standardize=FALSE,intercept=TRUE) 
+  fit2<-glmnet(X2,y2,family="binomial",standardize=FALSE,intercept=TRUE,lambda=(cvfit2$lambda.min))
+  tmp2=as.numeric(coef(fit2))[-1]
+  
+  cvfit3<-cv.glmnet(X3,y3,family="gaussian",standardize=FALSE,intercept=TRUE) 
+  fit3<-glmnet(X3,y3,family="gaussian",standardize=FALSE,intercept=TRUE,lambda=(cvfit3$lambda.min))
+  tmp3=as.numeric(coef(fit3))[-1]
+  
+  cvfit4<-cv.glmnet(X4,y4,family="binomial",standardize=FALSE,intercept=TRUE) 
+  fit4<-glmnet(X4,y4,family="binomial",standardize=FALSE,intercept=TRUE,lambda=(cvfit4$lambda.min))
+  tmp4=as.numeric(coef(fit4))[-1]
+  
+  cvfit5<-cv.glmnet(X5,y5,family="binomial",standardize=FALSE,intercept=TRUE) 
+  fit5<-glmnet(X5,y5,family="binomial",standardize=FALSE,intercept=TRUE,lambda=(cvfit5$lambda.min))
+  tmp5=as.numeric(coef(fit5))[-1]
+  
+  
+  Z1=abs(tmp1[1:N])
+  Z1tilde=abs(tmp1[N+1:N])
+  Z2=abs(tmp2[1:N])
+  Z2tilde=abs(tmp2[N+1:N])
+  Z3=abs(tmp3[1:N])
+  Z3tilde=abs(tmp3[N+1:N])
+  Z4=abs(tmp4[1:N])
+  Z4tilde=abs(tmp4[N+1:N])
+  Z5=abs(tmp5[1:N])
+  Z5tilde=abs(tmp5[N+1:N])
+  
+}
+
+  return(cbind(Z1,Z1tilde,Z2,Z2tilde,Z3,Z3tilde,Z4,Z4tilde,Z5,Z5tilde))
+}
+
+
+myresult <-function(coeff,q,choose,method,p){
+  ### p is the number of group
+  Z1=coeff[,1]
+  Z1tilde=coeff[,2]
+  Z2=coeff[,3]
+  Z2tilde=coeff[,4]
+  Z3=coeff[,5]
+  Z3tilde=coeff[,6]
+  Z4=coeff[,7]
+  Z4tilde=coeff[,8]
+  Z5=coeff[,9]
+  Z5tilde=coeff[,10]
+  if (choose=="grlasso"){
+	 if (method=="Diff"){
+	   W=(Z1-Z1tilde)*(Z2-Z2tilde)*(Z3-Z3tilde)*(Z4-Z4tilde)*(Z5-Z5tilde)
+	 }
+
+	mythred=knockoff.threshold(W,fdr=q,offset=1)
+	myselect=which(W>=mythred)
+  }
+  
+  if (choose=="lasso"){
+    if (method=="Diff"){
+      W=(Z1-Z1tilde)*(Z2-Z2tilde)*(Z3-Z3tilde)*(Z4-Z4tilde)*(Z5-Z5tilde)
+    }
+    mythred=knockoff.threshold(W,fdr=q,offset=1)
+    myselect=which(W>=mythred)
+    
+    N=length(Z1)
+    p=p #the number of group
+    pi=N/p
+    np<-rep(pi,p)
+    cnp<-c(0,cumsum(np))
+    
+    myselect1<-rep(0,p)
+    for (i in 1:p){
+      myselect1[i]<-length(which(myselect%in%((cnp[i]+1):cnp[i+1])))>0
+    }
+    myselect<-which(myselect1>0)
+}
+	return(myselect)
+	#list(myselect=myselect,Z=Z,Ztilde=Ztilde,Z1=Z1,Z1tilde=Z1tilde,Z2=Z2,Z2tilde=Z2tilde)
+}
---
+++ Simulation/K=5/change_rho/compKnock_mixed.R
@@ -0,0 +1,159 @@
+library("magic")
+library("mvtnorm")
+library("lars")
+library("knockoff")
+library("grpreg")
+library("far")
+library("methods")
+library("glmnet")
+library(seqknockoff)
+
+generateZ<-function(fit){
+  # p is the number of group
+  res<-abs(fit$beta[-1,])>0
+  gg <- as.numeric(fit$group)
+  N <- dim(res)[1]/2 #the number of predictors
+  p=max(gg)/2 #the number of group
+  np<-as.vector(table(gg))[1:p]#the number of predictors in each group
+  cnp<-c(0,cumsum(np))
+  
+  ###decide how many variables are nonzero within each group for each lamda
+  ee<-apply(res,2,cumsum)
+  dd<-ee[c(cnp[-1],cnp[-1]+N),]-rbind(0,ee[c(cnp[-(p+1)],cnp[-(p+1)]+N),])
+  ###find the best beta
+  tmpf<-function(v){
+    s<-0
+    a<-which(v>0)
+    if (length(a)>0){
+      s<-fit$lambda[min(a)]
+    }
+    s
+  }
+  tmp<-as.vector(apply(dd,1,tmpf))
+  #W<-(tmp[1:p]+tmp[p+1:p])*((tmp[1:p]-tmp[p+1:p])>0)-tmp[p+1:p]
+  tmp
+}
+
+####fitting
+
+myest<-function(datal){
+	##method Max: sign max
+	##method Diff: difference
+  data<-datal$data
+  group<-datal$group1
+  
+  data1<-data[which(data$C==1),]
+  data2<-data[which(data$C==2),]
+  data3<-data[which(data$C==3),]
+  data4<-data[which(data$C==4),]
+  data5<-data[which(data$C==5),]
+ 
+  p=max(datal$group1) ### how many group
+  X1<-data1[,-c(1:2)]
+  X2<-data2[,-c(1:2)]
+  X3<-data3[,-c(1:2)]
+  X4<-data4[,-c(1:2)]
+  X5<-data5[,-c(1:2)]
+  
+  
+  y1<-data1[,2]
+  y2<-data2[,2]
+  y3<-data3[,2]
+  y4<-data4[,2]
+  y5<-data5[,2]
+  
+  X1Knock<-group_seq_knock(X1,group)
+  X2Knock<-group_seq_knock(X2,group)
+  X3Knock<-group_seq_knock(X3,group)
+  X4Knock<-group_seq_knock(X4,group)
+  X5Knock<-group_seq_knock(X5,group)
+  
+  ###group
+  which.factor <- as.numeric(which(sapply(X1, is.factor)))
+  LL <- nlevels(X1[,which.factor[1]]) ##the levels for each column are the same currently
+  group_cate=rep(group[which.factor],each=LL-1) ### if the group size are different, the number should be changed
+  groupx <- c(group_cate, group[-which.factor])
+  group <-groupx[order(groupx)]
+  
+  ### using group lasso for the coefficient
+  X1C <- model.matrix( ~ ., X1)[, -1]
+  X1KnockC <- model.matrix( ~ ., X1Knock)[, -1]
+  X2C <- model.matrix( ~ ., X2)[, -1]
+  X2KnockC <- model.matrix( ~ ., X2Knock)[, -1]
+  X3C <- model.matrix( ~ ., X3)[, -1]
+  X3KnockC <- model.matrix( ~ ., X3Knock)[, -1]
+  X4C <- model.matrix( ~ ., X4)[, -1]
+  X4KnockC <- model.matrix( ~ ., X4Knock)[, -1]
+  X5C <- model.matrix( ~ ., X5)[, -1]
+  X5KnockC <- model.matrix( ~ ., X5Knock)[, -1]
+  
+  fit1<-grpreg(as.matrix(cbind(X1C,X1KnockC)),y1,group=c(group,group+p),penalty="grLasso",family="gaussian",nlambda=1000)
+  fit2<-grpreg(as.matrix(cbind(X2C,X2KnockC)),y2,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+  fit3<-grpreg(as.matrix(cbind(X3C,X3KnockC)),y3,group=c(group,group+p),penalty="grLasso",family="gaussian",nlambda=1000)
+  fit4<-grpreg(as.matrix(cbind(X4C,X4KnockC)),y4,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+  fit5<-grpreg(as.matrix(cbind(X5C,X5KnockC)),y5,group=c(group,group+p),penalty="grLasso",family="binomial",nlambda=1000)
+  
+  Z1=abs(generateZ(fit1)[(1:p)])
+  Z1tilde=abs(generateZ(fit1)[p+(1:p)])
+  Z2=abs(generateZ(fit2)[(1:p)])
+  Z2tilde=abs(generateZ(fit2)[p+(1:p)])
+  Z3=abs(generateZ(fit3)[(1:p)])
+  Z3tilde=abs(generateZ(fit3)[p+(1:p)])
+  Z4=abs(generateZ(fit4)[(1:p)])
+  Z4tilde=abs(generateZ(fit4)[p+(1:p)])
+  Z5=abs(generateZ(fit5)[(1:p)])
+  Z5tilde=abs(generateZ(fit5)[p+(1:p)])
+  return(cbind(Z1,Z1tilde,Z2,Z2tilde,Z3,Z3tilde,Z4,Z4tilde,Z5,Z5tilde))
+}
+
+myresult <-function(coeff,q,choose,method){
+  Z1=coeff[,1]
+  Z1tilde=coeff[,2]
+  Z2=coeff[,3]
+  Z2tilde=coeff[,4]
+  Z3=coeff[,5]
+  Z3tilde=coeff[,6]
+  Z4=coeff[,7]
+  Z4tilde=coeff[,8]
+  Z5=coeff[,9]
+  Z5tilde=coeff[,10]
+  if (choose=="simultaneous"){
+    if (method=="Diff"){
+      W=(Z1-Z1tilde)*(Z2-Z2tilde)*(Z3-Z3tilde)*(Z4-Z4tilde)*(Z5-Z5tilde)
+    }
+    mythred=knockoff.threshold(W,fdr=q,offset=1)
+    myselect=which(W>=mythred)
+  }
+  
+  if (choose=="intersection"){
+    if (method=="Max"){
+      W1=pmax(Z1,Z1tilde)*(-1)^(Z1<=Z1tilde)
+      W2=pmax(Z2,Z2tilde)*(-1)^(Z2<=Z2tilde)
+      W3=pmax(Z3,Z3tilde)*(-1)^(Z3<=Z3tilde)
+      W4=pmax(Z4,Z4tilde)*(-1)^(Z4<=Z4tilde)
+      W5=pmax(Z5,Z5tilde)*(-1)^(Z5<=Z5tilde)
+    }
+    if (method=="Diff"){
+      W1=Z1-Z1tilde
+      W2=Z2-Z2tilde
+      W3=Z3-Z3tilde
+      W4=Z4-Z4tilde
+      W5=Z5-Z5tilde
+    }
+    mythred1=knockoff.threshold(W1,fdr=q,offset=1)
+    myselect1=which(W1>=mythred1)
+    mythred2=knockoff.threshold(W2,fdr=q,offset=1)
+    myselect2=which(W2>=mythred2)
+    mythred3=knockoff.threshold(W3,fdr=q,offset=1)
+    myselect3=which(W3>=mythred3)
+    mythred4=knockoff.threshold(W4,fdr=q,offset=1)
+    myselect4=which(W4>=mythred4)
+    mythred5=knockoff.threshold(W5,fdr=q,offset=1)
+    myselect5=which(W5>=mythred5)
+    
+    myselect=intersect(intersect(intersect(intersect(myselect1,myselect2),myselect3),myselect4),myselect5)
+  }
+  
+  return(myselect)
+  #list(myselect=myselect,Z=Z,Ztilde=Ztilde,Z1=Z1,Z1tilde=Z1tilde,Z2=Z2,Z2tilde=Z2tilde)
+}
