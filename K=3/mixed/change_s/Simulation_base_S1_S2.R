45df7c9c9def84bf3dc68c0452eb683d7cbdcb88
Runqiu Wang <37877117+RunqiuWang22@users.noreply.github.com>
2024-01-18 20:47:20 -0600

    Add files via upload

---
+++ K=3/mixed/change_s/Simulation_base_S1_S2.R
@@ -0,0 +1,57 @@
+
+args=commandArgs(trailingOnly=TRUE)
+iii=as.numeric(args[1])
+
+library("magic")
+library("mvtnorm")
+library("lars")
+library("knockoff")
+library("grpreg")
+library("far")
+library("methods")
+library("doParallel")
+library("glmnet")
+
+####two scenario of signal strengths: 
+samesig_list=c(0,1)
+samesig=samesig_list[iii%%length(samesig_list)+1]
+iii=iii%/%length(samesig_list)
+
+#### different scale:previous one is 1.2
+scale_list=c(2)##length is 5
+scale=scale_list[iii%%length(scale_list)+1]
+iii=iii%/%length(scale_list)
+
+###different dimension N
+N_list=c(160) ##length is 2
+N=N_list[iii%%length(N_list)+1]
+iii=iii%/%length(N_list)
+
+#ps0=0.2
+ps1_list=seq(0.05,0.2,by=0.025) ##length is 4
+ps1=ps1_list[iii%%length(ps1_list)+1]
+iii=iii%/%length(ps1_list)
+
+ps3=ps2=ps1
+#####baseline setting
+pb=0.25 #proportion of categorical variables
+LL = 3
+
+n1=1000
+n2=1000
+n3=1000
+pi=4
+ps0=0.3
+ps12=ps23=0.05
+ps13=0
+q=0.2
+rho1=rho2=rho3=0.5
+gamma1=gamma2=gamma3=0.1
+sigma1=1
+sigma2=2
+sigma3=1
+iniseed=1111
+M=100
+setwd("./")
+filename=paste("Simulation_base_mixed",as.character(iii),".R",sep="")
+source(filename)
